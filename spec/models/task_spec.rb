8114cbc79453f394afea5ef709edc151f27c83e2
triggerwoods <keithflint@inbox.ru>
2012-03-19 13:05:17 +0400

    refactor specs

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -3,16 +3,14 @@ require 'spec_helper.rb'
 describe Task do
 
   describe 'validations' do
-    before :each do
+    before :all do
       @attr = {:title => "do something!", :content => "don't be lazzy!", :do_before => Date.today}
     end
     it "should have a title" do
-      @task = Task.new(@attr.merge(:title =>''))
-      @task.should_not be_valid
+      Task.new(@attr.merge(:title =>'')).should_not be_valid
     end
     it "should have a 'do before' date" do
-      @task = Task.new(@attr.merge(:do_before => nil))
-      @task.should_not be_valid
+      Task.new(@attr.merge(:do_before => nil)).should_not be_valid
     end
     it "normal task usually should be valid" do
        Task.new(@attr).should be_valid
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,28 +4,28 @@ describe User do
 
   describe "validations" do
 
-    before :each do
+    before :all do
       @attr = {:name => "jack", :email => "some@email.com"}
     end
 
     it 'user should have a name' do
-      @user = User.new(@attr.merge(:name => ''))
-      @user.should_not be_valid
+      User.new(@attr.merge(:name => '')).should_not be_valid
     end
 
     it 'user should have a email' do
-      @user = User.new(@attr.merge(:email => ''))
-      @user.should_not be_valid
+      User.new(@attr.merge(:email => '')).should_not be_valid
     end
 
     it 'should reject names that are too long' do
-      @user = User.new(@attr.merge(:name => 'x'*26))
-      @user.should_not be_valid
+      User.new(@attr.merge(:name => 'x'*26)).should_not be_valid
     end
 
     it 'user should have a normal email' do
-      @user = User.new(@attr.merge(:email => 'qwerty@.ru'))
-      @user.should_not be_valid
+      User.new(@attr.merge(:email => 'qwerty@.ru')).should_not be_valid
+    end
+
+    it 'normal user usually should be valid' do
+      User.new(@attr).should be_valid
     end
 
   end
